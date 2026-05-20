083c48779218d8fd5e5ee84f2b80847cc5633999
mamichakes-alt <mamichakes@gmail.com>
2026-05-20 13:32:49 -0600

    actualizar

---
+++ script.js
@@ -0,0 +1,23 @@
+function mostrarMensaje() {
+    let mensaje = document.getElementById("mensaje");
+
+    mensaje.innerHTML = "Cuida y protege a los animales.";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity = "1";
+        mensaje.style.transform = "translateY(0)";
+    }, 50);
+}
+
+function mostrarMensaje2() {
+    let mensaje = document.getElementById("mensaje2");
+
+    mensaje.innerHTML = "Hola, soy Oscar.";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity = "1";
+        mensaje.style.transform = "translateY(0)";
+    }, 50);
+}
