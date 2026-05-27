6f47f25de2c393a030f27f76b8c47d1aa276fdef
mamichakes-alt <mamichakes@gmail.com>
2026-05-27 10:28:51 -0600

    cambios

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ function mostrarMensaje2() {
         mensaje.style.transform = "translateY(0)";
     }, 50);
 }
+function mostrarMensaje2() {
+    let mensaje = document.getElementById("mensaje2");
+
+    mensaje.innerHTML = "Hola, soy Oscar.";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity = "1";
+        mensaje.style.transform = "translateY(0)";
+    }, 50);
+}
